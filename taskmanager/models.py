0db6573da0e30c02477b652ef1f3f0decf0798f0
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-01-30 13:46:31 +0000

    added the categories and add_categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,18 +2,15 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        
         return self.category_name
 
 
 class Task(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -22,7 +19,6 @@ class Task(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        
         return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
