e078dea992c5f3a4aa20aaaebced4132596d49c7
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-01-30 14:48:20 +0000

    adding the tasks and add_task html and functions

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  })
+document.addEventListener("DOMContentLoaded", function() {
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+});
